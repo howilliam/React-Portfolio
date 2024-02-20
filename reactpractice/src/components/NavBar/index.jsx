fc9b99464fefcd2e8ae359d841dab6522fcce014
howarceus <howarceus@gmail.com>
2024-02-20 20:52:17 +0000

    changes made

--- reactpractice/src/components/NavBar/index.jsx
+++ reactpractice/src/components/NavBar/index.jsx
@@ -5,7 +5,7 @@ const NavigationBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/Home">
+        <a className="navbar-brand" href="/">
           William Ho
         </a>
         <button
@@ -23,7 +23,7 @@ const NavigationBar = () => {
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
               <NavLink
-                to="/Home"
+                to="/"
                 end
                 className={({ isActive }) =>
                   isActive ? "nav-link active" : "nav-link"
